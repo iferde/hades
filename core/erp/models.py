61eebf3b25c9b41cebfc653d34934e7998225558
iferde <isaiasfm_dev@gmail.comgit config --global user.email isaiasfm_dev@gmail.comgit config --global user.email isaiasfm_dev@gmail.com>
2024-03-15 19:03:24 +0100

    CRUD de Category, inclusión librería datatable y función JavaScript.

--- core/erp/models.py
+++ core/erp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.forms import model_to_dict
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Nombre', unique=True)
+    desc = models.CharField(max_length=500, null=True, blank=True, verbose_name='Descripción')
+
+    def __str__(self):
+        return self.name
+
+    def toJSON(self):
+        item = model_to_dict(self)
+        return item
+
+    class Meta:
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
+        ordering = ['id']
+
+
--- core/erp/urls.py
+++ core/erp/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 from core.erp.views.dashboard.views import *
+from core.erp.views.category.views import *
 
 app_name = 'erp'
 
 urlpatterns = [
     # Home
     path('dashboard/', dashboard_TemplateView.as_view(), name='dashboard'),
+    # Category
+    path('category/list/', CategoryListView.as_view(), name='category_list'),
+    path('category/list2/', category_list, name='category_list2'),
+    path('category/add/', CategoryCreateView.as_view(), name='category_create'),
+    path('category/edit/<int:pk>/', CategoryUpdateView.as_view(), name='category_update'),
+    path('category/delete/<int:pk>/', CategoryDeleteView.as_view(), name='category_delete'),
 ]
-
