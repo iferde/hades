8210a86377bff334bec90cfa4fd336175eca3e5d
iferde <isaiasfm_dev@gmail.comgit config --global user.email isaiasfm_dev@gmail.comgit config --global user.email isaiasfm_dev@gmail.com>
2024-03-24 12:08:37 +0100

    Edición y Borrado de entidad Clientes con Model y Ajax.

--- core/erp/static/client/js/list.js
+++ core/erp/static/client/js/list.js
@@ -1,4 +1,5 @@
 var tbClient;
+var modal_title;
 
 function getData() {
     tbClient = $('#data').DataTable({
@@ -20,7 +21,7 @@ function getData() {
             {"data": "surnames"},
             {"data": "dni"},
             {"data": "date_birthday"},
-            {"data": "gender"},
+            {"data": "gender.name"},
             {"data": "id"},
         ],
         columnDefs: [
@@ -29,8 +30,8 @@ function getData() {
                 class: 'text-center',
                 orderable: false,
                 render: function (data, type, row) {
-                    var buttons = '<a href="#" class="btn btn-warning btn-xs btn-flat"><i class="fas fa-edit"></i></a> ';
-                    buttons += '<a href="#" type="button" class="btn btn-danger btn-xs btn-flat"><i class="fas fa-trash-alt"></i></a>';
+                    var buttons = '<a href="#" rel="edit" class="btn btn-warning btn-xs btn-flat"><i class="fas fa-edit"></i></a> ';
+                    buttons += '<a href="#" rel="delete" type="button" class="btn btn-danger btn-xs btn-flat"><i class="fas fa-trash-alt"></i></a>';
                     return buttons;
                 }
             },
@@ -43,14 +44,54 @@ function getData() {
 $(function () {
     getData();
 
+    modal_title = $('.modal-title');
+
     $(".btnAdd").on('click', function() {
         $('input[name="action"]').val('add');
-        //$('form')[0].reset(); // Limpiar el formulario
+        modal_title.find('span').html('Creación de un cliente');
+        //console.log(modal_title.find('i'));
+        modal_title.find('i').removeClass().addClass('fas fa-plus');
+        $('form')[0].reset();
         $('#myModalClient').modal('show');
     })
+
+    $('#data tbody')
+        .on('click', 'a[rel="edit"]', function() {
+            var tr = tbClient.cell($(this).closest('td, li')).index();
+            //console.log(tr);
+            var data = tbClient.row(tr.row).data();
+            //console.log(data);
+            $('input[name="action"]').val('edit');
+            modal_title.find('span').html('Modificación de un cliente');
+            modal_title.find('i').removeClass().addClass('fas fa-edit');
+            $('input[name="id"]').val(data.id);
+            $('input[name="names"]').val(data.names);
+            $('input[name="surnames"]').val(data.surnames);
+            $('input[name="dni"]').val(data.dni);
+            $('input[name="date_birthday"]').val(data.date_birthday);
+            $('input[name="address"]').val(data.address);
+            $('select[name="gender"]').val(data.gender.id);
+            $('#myModalClient').modal('show');
+        })
+        .on('click', 'a[rel="delete"]', function() {
+            var tr = tbClient.cell($(this).closest('td, li')).index();
+            var data = tbClient.row(tr.row).data();
+            console.log(data);
+            var parameters = new FormData();
+            parameters.append('action', 'delete');
+            parameters.append('id', data.id);    
+            submit_with_ajax(
+            window.location.pathname,
+            "Notificación",
+            "¿Estas seguro de eliminar este registro?",
+            parameters,
+            function () {
+                tbClient.ajax.reload();
+            });
+        });
     
     $("#myModalClient").on('show.bs.modal', function() {
-        $('form')[0].reset(); // Limpiar el formulario
+        //$('form')[0].reset(); // Limpiar el formulario
     })  
 
     $("form").on("submit", function (e) {
--- core/erp/views/client/views.py
+++ core/erp/views/client/views.py
@@ -34,6 +34,18 @@ class ClientView(TemplateView):
                 cli.address = request.POST['address']
                 cli.gender = request.POST['gender']
                 cli.save()
+            elif action=='edit':
+                cli=Client.objects.get(pk=request.POST['id'])
+                cli.names = request.POST['names']
+                cli.surnames = request.POST['surnames']
+                cli.dni = request.POST['dni']
+                cli.date_birthday = request.POST['date_birthday']
+                cli.address = request.POST['address']
+                cli.gender = request.POST['gender']
+                cli.save()
+            elif action=='delete':
+                cli=Client.objects.get(pk=request.POST['id'])
+                cli.delete()
             else:
                 data['error'] = 'Ha ocurrido un error'
         except Exception as e:
